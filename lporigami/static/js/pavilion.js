078b30c0085223441107d2b5fcc1f720f4fd70d5
LERA\anambiar <amit.nambiar@lera.com>
2017-02-28 14:02:09 -0500

    working orbit controls

--- lporigami/static/js/pavilion.js
+++ lporigami/static/js/pavilion.js
@@ -1,20 +1,38 @@
 
 
+var pointsGroup = new THREE.Group();
+
+
+
+
+
+
+
+
 
 
 function pavilionRun(){
-  // Box
-  var box = new Physijs.BoxMesh(
-    new THREE.CubeGeometry( 5, 5, 5 ),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-  );
-  box.position.y = 20;
-  scene.add( box );
+  var spacing = 6;
+
+  for (var i=0; i<3; i++) {
+    for (var j=0; j<5; j++) {
+      // Box
+      var box = new Physijs.BoxMesh(
+        new THREE.CubeGeometry( 1, 1, 1 ),
+        new THREE.MeshBasicMaterial({ color: 0xff0000 })
+      );
+      box.position.x = i * spacing;
+      box.position.y = 10;
+      box.position.z = j * spacing;
+      scene.add( box );
+    }
+  }
+  // scene.add( pointsGroup );
 
 
   // Plane
   var plane = new Physijs.PlaneMesh(
-    new THREE.PlaneGeometry( 20, 20 ),
+    new THREE.PlaneGeometry( 60, 30 ),
     new THREE.MeshBasicMaterial({ color: 0xffffff })
   );
   plane.rotation.x = -Math.PI / 2;
--- lporigami/static/js/physiJSSceneSetup.js
+++ lporigami/static/js/physiJSSceneSetup.js
@@ -8,8 +8,18 @@ function boilerplatePhysiJS(document, workerPath, ammoPath) {
     Physijs.scripts.worker = workerPath;
     Physijs.scripts.ammo = ammoPath;
 
-    var initScene, render, renderer;
+    var initScene, renderer;
+    var raycaster, mouse;
 
+
+    function onMouseMove( event ) {
+        // calculate mouse position in normalized device coordinates
+        // (-1 to +1) for both components
+        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    }
+
+    
     initScene = function() {
         renderer = new THREE.WebGLRenderer({ antialias: true });
         renderer.setSize( window.innerWidth, window.innerHeight );
@@ -21,20 +31,41 @@ function boilerplatePhysiJS(document, workerPath, ammoPath) {
             35,
             window.innerWidth / window.innerHeight,
             1,
-            1000
+            10000
         );
         camera.position.set( 60, 50, 60 );
         camera.lookAt( scene.position );
         scene.add( camera );
 
-        requestAnimationFrame( render );
+        raycaster = new THREE.Raycaster();
+        raycaster.lineprecision = 0.00001;
+        raycaster.precision = 0.001;
+
+        mouse = new THREE.Vector2();
+        window.addEventListener( 'mousemove', onMouseMove, false );
     };
 
-    render = function() {
-        scene.simulate(); // run physics
-        renderer.render( scene, camera); // render the scene
-        requestAnimationFrame( render );
-    };
 
-    window.onload = initScene();
+
+    function renderScene(){
+        var controls = new THREE.OrbitControls(camera, renderer.domElement);
+        controls.target.set( 0, 0, 0 );
+        function render() {
+            // update the picking ray with the camera and mouse position
+            raycaster.setFromCamera( mouse, camera );
+            scene.simulate(); // run physics
+            renderer.render( scene, camera );
+            requestAnimationFrame( render );
+        }
+        render();
+    }
+
+
+    function initAndRenderScene() {
+        initScene();
+        renderScene();
+    }
+
+
+    window.onload = initAndRenderScene();
 }
