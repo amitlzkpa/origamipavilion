06b9018da4296501afae3db7eb2979aa3ea6608d
Nidhi Sekhar <nsekhar@lera.local>
2017-03-02 13:10:07 -0500

    non working slider joints

--- lporigami/static/js/pavilion.js
+++ lporigami/static/js/pavilion.js
@@ -96,11 +96,11 @@ function addOrigin() {
 
 
 function ControlPoint(row, col, xSpc, zSpc) {
-  this.phyMesh = getBox(1, 1, 1, 1, 0xff0000);
+  this.phyMesh = (row%2 != 0) ? getBox(0.1, 0.1, 0.1, 1, 0xff0000) : getBox(0.1, 0.1, 0.1, 5, 0xff0000);
   this.row = row;
   this.col = col;
   this.phyMesh.position.x = row * xSpc;
-  this.phyMesh.position.y = (col % 2==0)?0.5:1;
+  this.phyMesh.position.y = (col % 2==0)?0.5:2;
   this.phyMesh.position.z = col * zSpc;
 }
 
@@ -123,8 +123,8 @@ ControlPoint.prototype.getPosition = function() {
 }
 
 
-ControlPoint.prototype.move = function(amt) {
-  this.phyMesh.setLinearVelocity(new THREE.Vector3(0, 0, amt));
+ControlPoint.prototype.move = function(movVector) {
+  this.phyMesh.setLinearVelocity(moveVector);
 }
 
 
@@ -209,23 +209,7 @@ function redrawConstraintLines() {
 
 
 
-function attachPoints(xCount, zCount) {
-  for (var col=0; col<zCount-1; col++) {
-    for (var row=0; row<xCount; row++) {
-      scene.addConstraint(getConstraintObject(pointsArray[col][row], pointsArray[col+1][row]));
-    }
-  }
-
-  for (var col=0; col<zCount; col++) {
-    for (var row=0; row<xCount-1; row++) {
-      scene.addConstraint(getConstraintObject(pointsArray[col][row], pointsArray[col][row+1]));
-    }
-  }
-}
-
-
-
-function getConstraintObjectSingle(cp1) {
+function getSinglePtConstraintObject(cp1) {
   var pt1 = cp1.getPhyMesh();
   var constraint = new Physijs.PointConstraint(
     pt1,
@@ -236,7 +220,7 @@ function getConstraintObjectSingle(cp1) {
 
 
 
-function getConstraintObject(cp1, cp2) {
+function getDoublePtConstraintObject(cp1, cp2) {
   var pt1 = cp1.getPhyMesh();
   var pt2 = cp2.getPhyMesh();
   var constraint = new Physijs.PointConstraint(
@@ -250,14 +234,87 @@ function getConstraintObject(cp1, cp2) {
 
 
 
+//---------------------------------------------------------
+
+
+
+
+function addPtConstraints(xCount, zCount) {
+  for (var col=0; col<zCount-1; col++) {
+    for (var row=0; row<xCount; row++) {
+      scene.addConstraint(getDoublePtConstraintObject(pointsArray[col][row], pointsArray[col+1][row]));
+    }
+  }
+
+  for (var col=0; col<zCount; col++) {
+    for (var row=0; row<xCount-1; row++) {
+      scene.addConstraint(getDoublePtConstraintObject(pointsArray[col][row], pointsArray[col][row+1]));
+    }
+  }
+}
+
+
+
+
+//-----------------------------------------------------------------------------
+
+
+
+function addSingleSliderConstraintObject(cp1, axis) {
+  var pt1 = cp1.getPhyMesh();
+  var constraint = new Physijs.SliderConstraint(
+    pt1,
+    new THREE.Vector3(0, 0, 0),
+    axis
+    );
+  scene.add(constraint);
+  constraint.setLimits(-10, 10, 0, 0);
+  constraint.setRestitution(0.1, 0.1);
+  // constraint.enableLinearMotor(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0) );
+  // constraint.disableLinearMotor();
+  // constraint.enableAngularMotor(new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 0, 0));
+  // constraint.disableAngularMotor();
+}
+
+
+
+
+//---------------------------------------------------------
+
+
+
+
+function addSliderConstraints(xCount, zCount) {
+  // for (var col=0; col<zCount-1; col++) {
+  //   for (var row=0; row<xCount; row++) {
+  //     var cp1 = pointsArray[col][row];
+  //     var cp2 = pointsArray[col+1][row];
+  //     var axis = new THREE.Vector3(0, 0, 1);
+  //     scene.addConstraint(getSingleSliderConstraintObject(cp1, axis, cp1.getPosition(), cp2.getPosition()));
+  //   }
+  // }
+
+  for (var col=0; col<zCount; col++) {
+    for (var row=0; row<xCount-1; row++) {
+      var cp1 = pointsArray[col][row];
+      var cp2 = pointsArray[col][row+1];
+      var axis = new THREE.Vector3(0, 1, 0);
+      addSingleSliderConstraintObject(cp1, axis);
+    }
+  }
+}
+
+
+
+
 //-----------------------------------------------------------------------------
 
 
 
 
-function moveLastRow(amt) {
+function moveLastRow(moveVector) {
   for(var i=0; i<pointsArray[pointsArray.length-1].length; i++) {
-    pointsArray[pointsArray.length-1][i].move(amt);
+    pointsArray[pointsArray.length-1][i].move(moveVector);
   }
 }
 
@@ -265,17 +322,17 @@ function moveLastRow(amt) {
 
 function pinDownFirstRow() {
   for(var i=0; i<pointsArray[0].length; i++) {
-    scene.addConstraint(getConstraintObjectSingle(pointsArray[0][i]));
+    scene.addConstraint(getSinglePtConstraintObject(pointsArray[0][i]));
   }
 }
 
 
 
-function moveAltPoint(amt) {
+function moveAltPoint(moveVector) {
   for(var i=2; i<pointsArray.length; i++) {
     if (i % 2 == 0) {
       for (var j=0; j<pointsArray[i].length; j++) {
-        pointsArray[i][j].getPhyMesh().setLinearVelocity(new THREE.Vector3(0, 0, amt));
+        pointsArray[i][j].getPhyMesh().setLinearVelocity(moveVector);
       }
     }
   }
@@ -283,13 +340,32 @@ function moveAltPoint(amt) {
 
 
 
-// function moveBullDozer(amt) {
-//   bullDozer.setLinearVelocity(new THREE.Vector3(0, 0, amt));
+
+//-----------------------------------------------------------------------------
+
+
+var trialBox;
+
+function trialSlider() {
+  trialBox = getBox(1, 1, 1, 1, 0xff00ff);
+  trialBox.position.set(-3, 3, -3);
+  scene.add(trialBox);
+  var constraint = new Physijs.SliderConstraint(trialBox, new THREE.Vector3(0, 0, 0), new THREE.Vector3(1, 0, 0));
+  scene.addConstraint(constraint);
+  constraint.setLimits(-10, 10, 0, 0);
+  constraint.setRestitution(0.1, 0.1);
+}
+
+
+
+// function moveBullDozer(moveVector) {
+//   bullDozer.setLinearVelocity(moveVector);
 // }
 
 
 
 // var bullDozer;
+
 
 
 // function addBullDozer() {
@@ -310,16 +386,34 @@ function moveAltPoint(amt) {
 
 function pavilionRun(){
 
+  var xCount = 3;
+  var zCount = 5;
+  var xSpacing = 6;
+  var zSpacing = 3;
+  if(debug) addOrigin();
+  addGroundPlane();
+  // addBullDozer();
+  addPointsArray(xCount, zCount, xSpacing, zSpacing);
+  addPtConstraints(xCount, zCount);
+  // addSliderConstraints(xCount, zCount);
+  pinDownFirstRow();
+
+  trialSlider();
+
+
+  var pushSpeed = 4;
+  var moveXVec = new THREE.Vector3(pushSpeed, 0, 0);
+  var moveYVec = new THREE.Vector3(0, pushSpeed, 0);
+  var moveZVec = new THREE.Vector3(0, 0, -pushSpeed);
+
+  var moveYZVec = new THREE.Vector3(0, pushSpeed*4, pushSpeed*4);
+
+
   window.addEventListener( 'keydown', function( event ) {
       switch ( event.keyCode ) {
         // Q
         case 81: {
-          moveLastRow(-10);
-          break;
-        }
-        // W
-        case 87: {
-          moveLastRow(10);
+          moveLastRow(moveZVec);
           break;
         }
         // D
@@ -329,27 +423,17 @@ function pavilionRun(){
         }
         // F
         case 70: {
-          moveAltPoint(-10);
+          moveAltPoint(moveZVec);
           break;
         }
-        // G
-        case 71: {
-          moveAltPoint(10);
+        // Z
+        case 90: {
+          trialBox.setLinearVelocity(moveYZVec);
           break;
         }
     }
   });
 
-  var xCount = 3;
-  var zCount = 5;
-  var xSpacing = 6;
-  var zSpacing = 3;
-  if(debug) addOrigin();
-  addGroundPlane();
-  // addBullDozer();
-  addPointsArray(xCount, zCount, xSpacing, zSpacing);
-  attachPoints(xCount, zCount);
-  pinDownFirstRow();
 }
 
 
